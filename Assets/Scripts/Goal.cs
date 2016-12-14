95cc22f285db4a2cc191dbd36a985c9364165291
CNParent <chrisnparent@gmail.com>
2016-12-14 09:56:58 -0500

    Initial commit

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+    public AudioSource goal;
+
+    void OnTriggerEnter(Collider c)
+    {
+        goal.Play(); // I don't think I'm forgetting anything...
+    }
+}
